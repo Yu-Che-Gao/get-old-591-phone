127683dce259653e45dc06ed0e951857ba1f1f20
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-07-29 16:31:14 +0800

    TODO: imageURL有問題

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const app = express();
 const request = require('request');
-const port = process.env.PORT || 80;
+const bodyParser=require('body-parser');
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
     res.send('you have no right to access this page.');
 });
 
 app.get('/get', function (req, res) {
-    request('https://' + req.params.imageURL, function (error, response, body) {
+    let imageURL=encodeURIComponent(req.query.imageURL);
+    //TODO: imageURL有問題
+    request('http://x.rce.tw/s/h3584935/get_old_591_phone.php?imageURL=' + imageURL, function (error, response, body) {
+        console.log('http://x.rce.tw/s/h3584935/get_old_591_phone.php?imageURL=' + imageURL);
         if (!error && response.statusCode == 200) {
             res.send(body) // Show the HTML for the Google homepage.
+        } else {
+            console.log(error);
         }
     });
 });
