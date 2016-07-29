ee4b7b56cbe77db7c13b6a9a1b8fdb0b2fdecd68
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-07-29 16:04:50 +0800

    改為80port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const request = require('request');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 app.get('/', function (req, res) {
     res.send('you have no right to access this page.');
